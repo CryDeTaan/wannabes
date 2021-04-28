0248911ad5b17d758a3239f3f54b705b90c4c9a9
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-04-28 20:09:37 +0100

    ✨ Home View added.

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <app-layout>
+        <div class="max-w-3xl mx-auto px-6 md:max-w-7xl md:px-8 md:grid md:grid-cols-12 lg:gap-8">
+            <main class="md:col-span-12 lg:col-span-9">
+                <div>
+                    Cards
+                </div>
+            </main>
+            <aside class="hidden lg:block lg:col-span-3">
+                <div class="sticky space-y-4">
+                    <div class="px-4 pt-2 bg-white rounded-lg shadow">
+                        <div class="text-gray-700 text-md font-medium">Leader Board:</div>
+                        <div class="grid grid-cols-1 divide-y divide-gray-200">
+                            Users
+                        </div>
+                    </div>
+                </div>
+            </aside>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout'
+
+export default {
+    components: {
+        AppLayout,
+    },
+}
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -16,12 +15,8 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Home');
+
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
