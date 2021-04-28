981d64eef2079cc801d602d90f5534af5ebcd02a
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-04-28 08:56:27 +0100

    🎨 Added icon to be used for this project

--- resources/js/Jetstream/AuthenticationCardLogo.vue
+++ resources/js/Jetstream/AuthenticationCardLogo.vue
@@ -1,8 +1,7 @@
 <template>
     <inertia-link :href="'/'">
-        <svg class="w-16 h-16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
-            <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 text-rose-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
         </svg>
     </inertia-link>
 </template>
