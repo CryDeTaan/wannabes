917fa4ddba856146d20ade18d41bbf90fcd519e6
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-04-30 04:15:24 +0100

    🎨 snippets should live in the view and should be passed to the component.

--- resources/js/Components/CardsGrid.vue
+++ resources/js/Components/CardsGrid.vue
@@ -58,197 +58,6 @@ import { PlusCircleIcon } from '@heroicons/vue/outline'
 import { FireIcon } from '@heroicons/vue/solid'
 import UserBlock from "@/Components/UserBlock";
 
-const snippets = [
-    {
-        slug: 'invoke-mimikatz',
-        title: 'Invoke-Mimikatz',
-        description: 'Dump Credentials and Secretes',
-        tags: [
-            {
-                'name' : 'PowerShell',
-                'color' : 'blue'
-            }
-        ],
-        likes: '192',
-        user: {
-            handle: 'CryDeTaan',
-            streetCred: 123,
-            imageUrl:
-                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-    {
-        slug: 'minidump-lsass',
-        title: 'MiniDump LSASS',
-        description: 'Dump LSASS using comsvcs.dll',
-        tags: [
-            {
-                'name' : 'Windows',
-                'color' : 'green'
-            }
-        ],
-        likes: '168',
-        user: {
-            handle: 'CryDeTaan',
-            streetCred: 123,
-            imageUrl:
-                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-    {
-        slug: 'disable-amsi',
-        title: 'Disable AMSI',
-        description: 'Bypass Antivirus for Powershell',
-        tags: [
-            {
-                'name' : 'Windows',
-                'color' : 'green'
-            },
-            {
-                'name' : 'PowerShell',
-                'color' : 'blue'
-            },
-            {
-                'name' : 'Lateral Movement',
-                'color' : 'pink'
-            }
-        ],
-        likes: '154',
-        user: {
-            handle: 'CryDeTaan',
-            streetCred: 123,
-            imageUrl:
-                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    }, {
-        slug: 'invoke-command-session',
-        title: 'Invoke-Command $session',
-        description: 'Invoke Powershell commands remotely',
-        tags: [
-            {
-                'name' : 'PowerShell',
-                'color' : 'blue'
-            }
-        ],
-        user: {
-            handle: 'CryDeTaan',
-            streetCred: 123,
-            imageUrl:
-                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        likes: '132',
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-    {
-        slug: 'reverse-dynamic-socks-proxy',
-        title: 'Reverse Dynamic Socks Proxy',
-        description: 'Expose network behind compromised Windows Host',
-        tags: [
-            {
-                'name' : 'SSH',
-                'color' : 'red'
-            }
-        ],
-        likes: '116',
-        user: {
-            handle: 'CryDeTaan',
-            streetCred: 123,
-            imageUrl:
-                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-    {
-        slug: 'impacket-wmi-pth',
-        title: 'Impacket WMI PTH',
-        description: 'WMI Pass-the-Hash using Impacket',
-        tags: [
-            {
-                'name' : 'Lateral Movement',
-                'color' : 'pink'
-            }
-        ],
-        likes: '102',
-        user: {
-            handle: 'CryDeTaan',
-            streetCred: 123,
-            imageUrl:
-                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-    {
-        slug: 'import-modules-web-client',
-        title: 'Import Modules - Web Client',
-        description: 'Import Modules using Powershell web client',
-        tags: [
-            {
-                'name' : 'PowerShell',
-                'color' : 'blue'
-            }
-        ],
-        likes: '98',
-        user: {
-            handle: 'CryDeTaan',
-            streetCred: 123,
-            imageUrl:
-                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-    {
-        slug: 'start-team-server',
-        title: 'Start Team Server',
-        description: 'Start Cobalt Strike Team server with malleable profile ',
-        tags: [
-            {
-                'name' : 'C2C',
-                'color' : 'yellow'
-            }
-        ],
-        likes: '87',
-        user: {
-            handle: 'CryDeTaan',
-            streetCred: 123,
-            imageUrl:
-                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-    {
-        slug: 'invoke-bloodhound',
-        title: 'Invoke-Bloodhound',
-        description: 'Start Bloodhound Collector using Powershell 1973 in a club, here we go again.',
-        tags: [
-            {
-                'name' : 'PowerShell',
-                'color' : 'blue'
-            }
-        ],
-        likes: '85',
-        user: {
-            handle: 'CryDeTaan',
-            streetCred: 123,
-            imageUrl:
-                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-    // More snippets...
-]
-
 export default {
     name: "CardsGrid",
 
@@ -258,9 +67,14 @@ export default {
         FireIcon,
     },
 
-    setup() {
+    props: {
+        snippets: Object,
+    },
+
+    setup(props) {
+        const snippets = props.snippets
         return {
-            snippets,
+            snippets
         }
     },
 }
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,7 +2,7 @@
     <app-layout>
         <div class="max-w-3xl mx-auto px-6 md:max-w-7xl md:px-8 md:grid md:grid-cols-12 lg:gap-8">
             <main class="md:col-span-12 lg:col-span-9">
-                <cards-grid />
+                <cards-grid :snippets="snippets"/>
             </main>
             <aside class="hidden lg:block lg:col-span-3">
                 <div class="sticky space-y-4">
@@ -25,6 +25,196 @@ import AppLayout from '@/Layouts/AppLayout'
 import CardsGrid from "@/Components/CardsGrid";
 import UserBlock from "@/Components/UserBlock";
 
+const snippets = [
+    {
+        slug: 'invoke-mimikatz',
+        title: 'Invoke-Mimikatz',
+        description: 'Dump Credentials and Secretes',
+        tags: [
+            {
+                'name' : 'PowerShell',
+                'color' : 'blue'
+            }
+        ],
+        likes: '192',
+        user: {
+            handle: 'CryDeTaan',
+            streetCred: 123,
+            imageUrl:
+                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    {
+        slug: 'minidump-lsass',
+        title: 'MiniDump LSASS',
+        description: 'Dump LSASS using comsvcs.dll',
+        tags: [
+            {
+                'name' : 'Windows',
+                'color' : 'green'
+            }
+        ],
+        likes: '168',
+        user: {
+            handle: 'CryDeTaan',
+            streetCred: 123,
+            imageUrl:
+                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    {
+        slug: 'disable-amsi',
+        title: 'Disable AMSI',
+        description: 'Bypass Antivirus for Powershell',
+        tags: [
+            {
+                'name' : 'Windows',
+                'color' : 'green'
+            },
+            {
+                'name' : 'PowerShell',
+                'color' : 'blue'
+            },
+            {
+                'name' : 'Lateral Movement',
+                'color' : 'pink'
+            }
+        ],
+        likes: '154',
+        user: {
+            handle: 'CryDeTaan',
+            streetCred: 123,
+            imageUrl:
+                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    }, {
+        slug: 'invoke-command-session',
+        title: 'Invoke-Command $session',
+        description: 'Invoke Powershell commands remotely',
+        tags: [
+            {
+                'name' : 'PowerShell',
+                'color' : 'blue'
+            }
+        ],
+        user: {
+            handle: 'CryDeTaan',
+            streetCred: 123,
+            imageUrl:
+                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        likes: '132',
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    {
+        slug: 'reverse-dynamic-socks-proxy',
+        title: 'Reverse Dynamic Socks Proxy',
+        description: 'Expose network behind compromised Windows Host',
+        tags: [
+            {
+                'name' : 'SSH',
+                'color' : 'red'
+            }
+        ],
+        likes: '116',
+        user: {
+            handle: 'CryDeTaan',
+            streetCred: 123,
+            imageUrl:
+                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    {
+        slug: 'impacket-wmi-pth',
+        title: 'Impacket WMI PTH',
+        description: 'WMI Pass-the-Hash using Impacket',
+        tags: [
+            {
+                'name' : 'Lateral Movement',
+                'color' : 'pink'
+            }
+        ],
+        likes: '102',
+        user: {
+            handle: 'CryDeTaan',
+            streetCred: 123,
+            imageUrl:
+                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    {
+        slug: 'import-modules-web-client',
+        title: 'Import Modules - Web Client',
+        description: 'Import Modules using Powershell web client',
+        tags: [
+            {
+                'name' : 'PowerShell',
+                'color' : 'blue'
+            }
+        ],
+        likes: '98',
+        user: {
+            handle: 'CryDeTaan',
+            streetCred: 123,
+            imageUrl:
+                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    {
+        slug: 'start-team-server',
+        title: 'Start Team Server',
+        description: 'Start Cobalt Strike Team server with malleable profile ',
+        tags: [
+            {
+                'name' : 'C2C',
+                'color' : 'yellow'
+            }
+        ],
+        likes: '87',
+        user: {
+            handle: 'CryDeTaan',
+            streetCred: 123,
+            imageUrl:
+                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    {
+        slug: 'invoke-bloodhound',
+        title: 'Invoke-Bloodhound',
+        description: 'Start Bloodhound Collector using Powershell 1973 in a club, here we go again.',
+        tags: [
+            {
+                'name' : 'PowerShell',
+                'color' : 'blue'
+            }
+        ],
+        likes: '85',
+        user: {
+            handle: 'CryDeTaan',
+            streetCred: 123,
+            imageUrl:
+                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    // More snippets...
+]
 const leaderBoard = [
     {
         handle: 'CryDeTaan',
@@ -55,6 +245,7 @@ export default {
 
     setup() {
         return {
+            snippets,
             leaderBoard,
         }
     },
