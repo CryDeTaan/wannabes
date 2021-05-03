26ec75e36aea57340f87728640186b935bf69184
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-03 13:51:12 +0100

    💄 Added 🌑 (dark mode) to modal components.

--- resources/js/Jetstream/DialogModal.vue
+++ resources/js/Jetstream/DialogModal.vue
@@ -1,18 +1,18 @@
 <template>
     <modal :show="show" :max-width="maxWidth" :closeable="closeable" @close="close">
         <div class="px-6 py-4">
-            <div class="text-lg">
+            <div class="text-lg dark:text-dark-200">
                 <slot name="title">
                 </slot>
             </div>
 
-            <div class="mt-4">
+            <div class="mt-4 dark:text-dark-300">
                 <slot name="content">
                 </slot>
             </div>
         </div>
 
-        <div class="px-6 py-4 bg-gray-100 text-right">
+        <div class="px-6 py-4 bg-gray-100 dark:bg-dark-600 text-right">
             <slot name="footer">
             </slot>
         </div>
