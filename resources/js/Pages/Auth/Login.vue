a8cd1e4322fcb8e808fb7cc164b44fcf2907f024
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-03 12:00:32 +0100

    💄 Added 🌑 (dark mode) to login page/component

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -24,12 +24,12 @@
             <div class="block mt-4">
                 <label class="flex items-center">
                     <jet-checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                    <span class="ml-2 text-sm text-gray-600 dark:text-dark-300 ">Remember me</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <inertia-link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                <inertia-link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 dark:text-dark-300 hover:text-gray-900 dark:hover:text-dark-400">
                     Forgot your password?
                 </inertia-link>
 
