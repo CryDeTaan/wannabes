fa56f8968ca659e49c54b2cde6ffd71d7f337002
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-03 12:40:16 +0100

    💄 Added 🌑 (dark mode) to button components

--- resources/js/Jetstream/Button.vue
+++ resources/js/Jetstream/Button.vue
@@ -1,5 +1,7 @@
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+    <button
+        :type="type"
+        class="inline-flex items-center px-4 py-2 bg-primary-600 border border-transparent rounded-md font-semibold text-xs text-neutral-200 dark:text-dark-200 uppercase tracking-widest hover:bg-primary-700 active:bg-primary-900 focus:outline-none focus:border-primary-900 focus:ring focus:ring-primary-300 disabled:opacity-25 transition">
         <slot></slot>
     </button>
 </template>
--- resources/js/Jetstream/SecondaryButton.vue
+++ resources/js/Jetstream/SecondaryButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition">
+    <button :type="type" class="inline-flex items-center px-4 py-2 bg-white dark:bg-dark-700 border border-primary-300 dark:border-dark-500 rounded-md font-semibold text-xs text-neutral-700 dark:text-dark-400 uppercase tracking-widest shadow-sm hover:text-neutral-500 dark:hover:bg-dark-600 dark:hover:text-dark-400 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 active:text-neutral-800 active:bg-neutral-50 disabled:opacity-25 transition">
         <slot></slot>
     </button>
 </template>
