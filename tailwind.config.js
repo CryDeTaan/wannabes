5d63d49e455cdd157eeefc5039998c0e48626904
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-03 10:53:21 +0100

    💄 Typography Dark theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,12 +22,82 @@ module.exports = {
                 neutral: colors.gray,
                 dark: colors.gray,
             },
+            typography: (theme) => ({
+                dark: {
+                    css: [
+                        {
+                            color: theme('colors.neutral.300'),
+                            '[class~="lead"]': {
+                                color: theme('colors.neutral.300'),
+                            },
+                            a: {
+                                color: theme('colors.primary.500'),
+                                '&:hover': {
+                                    color: theme('colors.primary.600'),
+                                },
+                            },
+                            strong: {
+                                color: theme('colors.neutral.300'),
+                            },
+                            'ol > li::before': {
+                                color: theme('colors.neutral.300'),
+                            },
+                            'ul > li::before': {
+                                backgroundColor: theme('colors.neutral.500'),
+                            },
+                            hr: {
+                                borderColor: theme('colors.neutral.400'),
+                            },
+                            blockquote: {
+                                color: theme('colors.neutral.200'),
+                                borderLeftColor: theme('colors.neutral.500'),
+                            },
+                            h1: {
+                                color: theme('colors.neutral.400'),
+                            },
+                            h2: {
+                                color: theme('colors.neutral.300'),
+                            },
+                            h3: {
+                                color: theme('colors.neutral.500'),
+                            },
+                            h4: {
+                                color: theme('colors.neutral.500'),
+                            },
+                            'figure figcaption': {
+                                color: theme('colors.neutral.400'),
+                            },
+                            code: {
+                                color: theme('colors.primary.200'),
+                                backgroundColor: theme('colors.neutral.700'),
+                            },
+                            'a code': {
+                                color: theme('colors.primary.200'),
+                            },
+                            'code::before': {
+                                content: '""'
+                            },
+                            'code::after': {
+                                content: '""'
+                            },
+                            thead: {
+                                color: theme('colors.primary.500'),
+                                borderBottomColor: theme('colors.neutral.400'),
+                            },
+                            'tbody tr': {
+                                borderBottomColor: theme('colors.neutral.600'),
+                            },
+                        },
+                    ],
+                },
+            }),
         },
     },
 
     variants: {
         extend: {
             opacity: ['disabled'],
+            typography: ['dark'],
         },
     },
 
