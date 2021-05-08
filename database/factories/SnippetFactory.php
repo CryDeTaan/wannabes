3e936b6577b00c8d29a1fbf6ec51d5e331da9a04
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-08 11:45:15 +0100

    🚧 Snippet wip

---
+++ database/factories/SnippetFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Snippet;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SnippetFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Snippet::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
