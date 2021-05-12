e312433986cb1fd497a4c2e943959ff9c8b7baed
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-12 08:05:27 +0100

    🐛 v-else required on smaller screen size

--- resources/js/Pages/Snippets/Show.vue
+++ resources/js/Pages/Snippets/Show.vue
@@ -45,7 +45,7 @@
                             <PencilAltIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
                             Edit
                         </button>
-                        <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md shadow-sm text-white dark:text-dark-200 bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-primary-500">
+                        <button v-else type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md shadow-sm text-white dark:text-dark-200 bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-primary-500">
                             <PlusCircleIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
                             Street Cred
                         </button>
