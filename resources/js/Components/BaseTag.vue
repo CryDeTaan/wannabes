08b9c88d4f0322c7c3896a3438acae21e137a2cc
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-30 12:10:05 +0100

    🎨 Wrapped Tag with inertia link.

--- resources/js/Components/BaseTag.vue
+++ resources/js/Components/BaseTag.vue
@@ -1,10 +1,12 @@
 <template>
-    <span
-        class="inline-flex rounded-full items-center m-1 px-3 py-0.5 text-sm font-medium"
-        :class="`bg-${tag.color}-200 dark:bg-${tag.color}-600 text-${tag.color}-700 dark:text-${tag.color}-200`"
-    >
-        {{ tag.name }}
-    </span>
+    <inertia-link :href="tag.slug">
+        <span
+            class="inline-flex rounded-full items-center m-1 px-3 py-0.5 text-sm font-medium"
+            :class="`bg-${tag.color}-200 dark:bg-${tag.color}-600 text-${tag.color}-700 dark:text-${tag.color}-200`"
+        >
+            {{ tag.name }}
+        </span>
+    </inertia-link>
 </template>
 
 <script>
