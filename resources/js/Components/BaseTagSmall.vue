839e70815c79310e1986b83be8f67ba7e81b4222
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-31 06:24:39 +0100

    ✨ Added new small base tag component.

---
+++ resources/js/Components/BaseTagSmall.vue
@@ -0,0 +1,27 @@
+<template>
+    <inertia-link :href="tag.slug">
+        <span
+            class="inline-flex rounded-full items-center m-1 px-2.5 py-0.5 text-xs font-medium"
+            :class="`bg-${tag.color}-200 dark:bg-${tag.color}-600 text-${tag.color}-700 dark:text-${tag.color}-200`"
+        >
+            {{ tag.name }}
+        </span>
+    </inertia-link>
+</template>
+
+<script>
+export default {
+    name: "BaseTagSmall",
+
+    props: {
+        tag: {
+            type: Object,
+            required: true
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
