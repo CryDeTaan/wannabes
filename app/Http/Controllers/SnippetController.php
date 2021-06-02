5a411d39c1c4d8161900710a453620cb453ac2df
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-06-02 06:18:22 +0100

    🛡 Added auth middleware to snippets resource controller.

--- app/Http/Controllers/SnippetController.php
+++ app/Http/Controllers/SnippetController.php
@@ -15,6 +15,7 @@ class SnippetController extends Controller
 
     public function __construct()
     {
+        $this->middleware('auth:sanctum')->except('show');
         $this->authorizeResource(Snippet::class, 'snippet');
     }
 
