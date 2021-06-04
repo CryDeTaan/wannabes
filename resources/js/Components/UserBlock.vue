af62129a45c74cecd13eb853c273beae92cc762f
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-06-04 05:32:42 +0100

    ✨ User block fixed and improvements.

--- resources/js/Components/UserBlock.vue
+++ resources/js/Components/UserBlock.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex py-2">
-        <a
-            :href="`users/${ user.name }`"
+        <inertia-link
+            :href="`/users/${ user.name }`"
             class="flex flex-1 inline-flex justify-between text-xs text-gray-700 dark:text-dark-400 font-medium hover:text-gray-500 dark:hover:text-dark-300"
         >
             <span class="flex items-center">
@@ -10,10 +10,9 @@
             </span>
             <span class="flex items-center">
                 <FireIcon class="w-5 h-5 text-gray-400 dark:text-dark-500" aria-hidden="true" />
-                <!-- TODO: Add dynamic street cred -->
                 <span class="ml-2">{{ user.streetcred }}</span>
             </span>
-        </a>
+        </inertia-link>
     </div>
 </template>
 
