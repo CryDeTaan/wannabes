568df292804cd6a6a5736435e003673117e71ff4
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-06-23 19:40:40 +0100

    🎨 Extracted the tag request to a component.

--- resources/js/Components/TagsCard.vue
+++ resources/js/Components/TagsCard.vue
@@ -7,47 +7,20 @@
             />
         </li>
     </ul>
-    <p class="block mt-3 text-sm font-medium text-gray-700 dark:text-dark-400">Suggest a tag:</p>
-    <div class="flex items-center">
-        <base-input
-            label=""
-            v-model="form.name"
-            id="tag"
-            type="text"
-        />
-        <base-button @click="submitTagRequest" :secondary="true" class="ml-2 mt-1">Submit</base-button>
-    </div>
-    <p v-if="form.errors.name" class="mt-2 text-sm text-red-600" id="error`">{{ form.errors.name }}</p>
+    <tag-request />
 </template>
 
 <script>
-import {useForm} from "@inertiajs/inertia-vue3";
 import BaseTag from "@/Components/BaseTag";
+import TagRequest from "@/Components/TagRequest";
 
 export default {
     name: "TagsCard",
-    components: {BaseTag},
+    components: {TagRequest, BaseTag},
     props: {
         tags: Array,
     },
 
-    setup() {
-        const form = useForm({
-            name: null,
-        })
-
-        function submitTagRequest() {
-            form.post(route('tags.request'), {
-                preserveScroll: true,
-                onSuccess: () => form.reset('name'),
-            })
-        }
-
-        return {
-            form,
-            submitTagRequest,
-        }
-    },
 }
 </script>
 
