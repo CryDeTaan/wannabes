23c7c99d79df5244f251225166fc74ac67773e97
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-07-28 06:04:18 +0100

    🗑 There is no need for a form here.

--- resources/js/Pages/Snippets/Comments/CommentCreate.vue
+++ resources/js/Pages/Snippets/Comments/CommentCreate.vue
@@ -5,21 +5,19 @@
                 <img class="h-10 w-10 rounded-full" :src="$page.props.user.profile_photo_url" alt="" />
             </div>
             <div class="min-w-0 flex-1">
-                <form action="#">
-                    <div>
-                        <base-text-area
-                            v-model="form.body"
-                            label="Comment"
-                            :label-src-only="true"
-                            id="text"
-                            rows="3"
-                            placeholder="Add a comment"
-                        />
-                    </div>
-                    <div class="mt-3 flex justify-end">
-                        <base-button @click="submitForm">Comment</base-button>
-                    </div>
-                </form>
+                <div>
+                    <base-text-area
+                        v-model="form.body"
+                        label="Comment"
+                        :label-src-only="true"
+                        id="text"
+                        rows="3"
+                        placeholder="Add a comment"
+                    />
+                </div>
+                <div class="mt-3 flex justify-end">
+                    <base-button @click="submitForm">Comment</base-button>
+                </div>
             </div>
         </div>
     </div>
